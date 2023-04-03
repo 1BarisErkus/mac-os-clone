d802c5fe9b2e90888d9ab5ac26aa1b3afe7c334c
Barış Erkuş <bariserkus1244@hotmail.com>
2023-04-03 08:54:27 +0300

    completed project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ProgressBarModule } from 'primeng/progressbar';
+import { BootComponent } from './boot/boot.component';
+import { LoginComponent } from './login/login.component';
+import {InputTextModule} from 'primeng/inputtext';
+import { MachineControlsComponent } from './machine-controls/machine-controls.component';
+
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    BootComponent,
+    LoginComponent,
+    MachineControlsComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProgressBarModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
